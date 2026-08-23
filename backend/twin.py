d216f010dbc2ba7cfd7710b91b72bda7d1178dcd
ignacioaj <ignacioatencia@gmail.com>
2026-08-23 10:34:29 +0200

    playground.py added for assistant isolated use

--- backend/twin.py
+++ backend/twin.py
@@ -1,7 +1,5 @@
 from openai import OpenAI
 from backend.context import TWIN_INSTRUCTIONS
-# from backend.tools import tools, handle_tool_calls
-# import gradio as gr
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -10,9 +8,6 @@ MODEL_NAME = "gpt-5.4-mini"
 MAX_HISTORY_MESSAGES = 20
 
 openai = OpenAI()
-
-
-# system = [{"role": "system", "content": TWIN_INSTRUCTIONS}]
 
 def chat(message, history):
     print(history, flush=True)
@@ -41,18 +36,6 @@ def chat(message, history):
         input=input_messages
     )
 
-    # while response.choices[0].finish_reason == "tool_calls":
-    #     message = response.choices[0].message
-    #     tool_calls = message.tool_callsF
-    #     results = handle_tool_calls(tool_calls)
-    #     messages.append(message)
-    #     messages.extend(results)
-    #     response = openai.chat.completions.create(
-    #         model=MODEL_NAME,
-    #         messages=messages,
-    #         tools=tools
-    #     )
-
     response_text = response.output_text
 
     updated_history = history + [
@@ -67,13 +50,3 @@ def chat(message, history):
     ]
 
     return response_text, updated_history
-
-
-#demo = gr.ChatInterface(
-#    fn=chat,
-#    title="IA² — Ignacio's Career Twin",
-#    description="Ask me anything about Ignacio's professional background.",
-#)
-
-#if __name__ == "__main__":
-#    demo.launch()
---
+++ playground.py
@@ -0,0 +1,11 @@
+import gradio as gr
+from backend.twin import chat
+
+demo = gr.ChatInterface(
+    fn=chat,
+    title="IA² — Ignacio's Career Twin",
+    description="Ask me anything about Ignacio's professional background.",
+)
+
+if __name__ == "__main__":
+    demo.launch()
